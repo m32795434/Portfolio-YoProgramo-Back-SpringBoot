a285e0ab18277b79b77eb231faf7d3bd11bbdf44
Manuel Augusto Bravard <manubravard@gmail.com>
2023-04-01 19:45:50 -0300

    working on services

--- Portfolio/src/main/java/com/ManuelBravard/Portfolio/service/SectionService.java
+++ Portfolio/src/main/java/com/ManuelBravard/Portfolio/service/SectionService.java
@@ -1,34 +1,41 @@
 package com.ManuelBravard.Portfolio.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ManuelBravard.Portfolio.model.Section;
+import com.ManuelBravard.Portfolio.repository.SectionRepository;
 
 public class SectionService implements ISectionService {
+
+    @Autowired
+    public SectionRepository secRepo;
 
     // 1.23.34
     @Override
     public List<Section> returnSections() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'returnSections'");
+        return secRepo.findAll();
     }
 
     @Override
     public void addSection(Section sec) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addSection'");
+        secRepo.save(sec);
     }
 
     @Override
     public void deleteSection(String id) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteSection'");
+        secRepo.deleteById(id);
     }
 
     @Override
-    public Section returnSection(String id) {
+    public Optional<Section> returnSection(String id) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'returnSection'");
+        return secRepo.findById(id);
     }
-
+    // 1.29
 }
