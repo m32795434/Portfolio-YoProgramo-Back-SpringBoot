c234d61cc038726ba2f45a506cf464b6ef206046
Manuel Augusto Bravard <manubravard@gmail.com>
2023-04-11 22:16:07 -0300

    updating dependencies & anotation removed

--- Portfolio/src/main/java/com/ManuelBravard/Portfolio/CorsPolicy.java
+++ Portfolio/src/main/java/com/ManuelBravard/Portfolio/CorsPolicy.java
@@ -1,14 +1,11 @@
 package com.ManuelBravard.Portfolio;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-@SpringBootApplication
+// @EnableWebMvc
 public class CorsPolicy implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
