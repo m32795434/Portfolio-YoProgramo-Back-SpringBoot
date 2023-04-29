b5ebe5a253d65a5a63e764a089882196fb388ace
Manuel Augusto Bravard <manubravard@gmail.com>
2023-04-29 16:13:52 -0300

    exp card entity level validation

--- Portfolio/src/main/java/com/ManuelBravard/Portfolio/model/ExperienceCard.java
+++ Portfolio/src/main/java/com/ManuelBravard/Portfolio/model/ExperienceCard.java
@@ -10,8 +10,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,22 +25,47 @@ public class ExperienceCard implements Cloneable {
 
     @Id
     // @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(length = 1)
+    @Column(nullable = false)
+    // @Digits(integer = 1, fraction = 0, message = "The card id should be lower
+    // than 9")
     @Min(value = 1, message = "The card id should be greater than 0")
     @Max(value = 9, message = "The card id should be lower than 9")
     private Integer id;
 
+    @Column(length = 150)
+    @Size(max = 150)
     private String imgSrc;
+
+    @Column(length = 100)
+    @Size(max = 100)
     private String imgAltEs;
+
+    @Column(length = 100)
+    @Size(max = 100)
     private String imgAltEn;
+
+    @Column(nullable = false)
+    @Min(value = 2010)
     private int startDateYear;
+
+    @Column(nullable = false)
+    @Min(value = 1)
     private int startDateMonth;
+
+    @Column(nullable = false)
+    @Min(value = 1)
     private int startDateDay;
+
     private int endDateYear;
     private int endDateMonth;
     private int endDateDay;
+
+    @Column(columnDefinition = "text")
     private String phEs;
+
+    @Column(columnDefinition = "text")
     private String phEn;
+
     @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "Section_id")
