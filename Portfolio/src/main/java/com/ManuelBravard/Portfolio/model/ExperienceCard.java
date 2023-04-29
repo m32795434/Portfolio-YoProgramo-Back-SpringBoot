af6e67f47e40b69fc009fbfe33e4d3be2b6dd2a1
Manuel Augusto Bravard <manubravard@gmail.com>
2023-04-29 19:09:04 -0300

    unnecessary dependencies

--- Portfolio/src/main/java/com/ManuelBravard/Portfolio/model/ExperienceCard.java
+++ Portfolio/src/main/java/com/ManuelBravard/Portfolio/model/ExperienceCard.java
@@ -4,13 +4,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
