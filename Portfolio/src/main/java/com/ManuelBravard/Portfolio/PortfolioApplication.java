753155e080c9bb514f71cba529ae319f8fdb3cce
Manuel Augusto Bravard <manubravard@gmail.com>
2023-05-06 18:19:14 -0300

    don't set any new admin and manager

--- Portfolio/src/main/java/com/ManuelBravard/Portfolio/PortfolioApplication.java
+++ Portfolio/src/main/java/com/ManuelBravard/Portfolio/PortfolioApplication.java
@@ -18,29 +18,31 @@ public class PortfolioApplication {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(
-			AuthenticationService service) {
-		return args -> {
-			var admin = RegisterRequest.builder()
-					.firstname("Admin")
-					.lastname("Admin")
-					.email("admin@mail.com")
-					.password("password")
-					.role(ADMIN)
-					.build();
-			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+	// @Bean
+	// public CommandLineRunner commandLineRunner(
+	// AuthenticationService service) {
+	// return args -> {
+	// var admin = RegisterRequest.builder()
+	// .firstname("Admin")
+	// .lastname("Admin")
+	// .email("admin@mail.com")
+	// .password("password")
+	// .role(ADMIN)
+	// .build();
+	// System.out.println("Admin token: " +
+	// service.register(admin).getAccessToken());
 
-			var manager = RegisterRequest.builder()
-					.firstname("Admin")
-					.lastname("Admin")
-					.email("manager@mail.com")
-					.password("password")
-					.role(MANAGER)
-					.build();
-			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+	// var manager = RegisterRequest.builder()
+	// .firstname("Admin")
+	// .lastname("Admin")
+	// .email("manager@mail.com")
+	// .password("password")
+	// .role(MANAGER)
+	// .build();
+	// System.out.println("Manager token: " +
+	// service.register(manager).getAccessToken());
 
-		};
-	}
+	// };
+	// }
 
 }
