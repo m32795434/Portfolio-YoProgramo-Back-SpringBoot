b463ada05d7f6a6b3e52dd4e67dde65573eb49e3
Manuel Augusto Bravard <manubravard@gmail.com>
2023-05-08 20:06:56 -0300

    no username to save new pass

--- Portfolio/src/main/java/com/ManuelBravard/Portfolio/model/UpdateUserAndPassObj.java
+++ Portfolio/src/main/java/com/ManuelBravard/Portfolio/model/UpdateUserAndPassObj.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateUserAndPassObj {
-    private String email;
+    // private String email;
     private String password;
 }
